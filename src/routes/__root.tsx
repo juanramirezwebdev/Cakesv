52fed9f401e9420938fa8fbf87dde0be97b47d6c
juanramirezwebdev <juanluisra11@gmail.com>
2024-01-28 17:42:39 +1300

    f commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,22 @@
+import {  Outlet, RootRoute } from '@tanstack/react-router';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+
+export const Route = new RootRoute({
+  component: RootComponent,
+});
+
+
+function RootComponent() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer/>
+      
+    </>
+  );
+}
+
+export default RootComponent;
